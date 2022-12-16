12cee7b4f7f8a8a204e13af29e16bc194a51915b
Hiep <hiep@HiepnoMacBook-Pro.local>
2022-12-16 14:43:55 +0700

    result

---
+++ amidPhoneShop/app/models/Validation.js
@@ -0,0 +1,27 @@
+const getELe = id => document.getElementById(id);
+export default class Validation{
+
+    valicationCheck(val,errId,mess) {
+        if (val === "") {
+            getELe(errId).style.display = "block";
+            getELe(errId).innerHTML = mess;
+            return false;
+        }
+        getELe(errId).style.display = "none";
+        getELe(errId).innerHTML = "";
+        return true;
+    }
+
+    selectionCheck = function (selId, errId, mess) {
+        if (getELe(selId).selectedIndex !== 0) {
+        //true
+            getELe(errId).style.display = "none";
+            getELe(errId).innerHTML = "";
+        return true;
+    }
+    //false
+        getELe(errId).style.display = "block";
+        getELe(errId).innerHTML = mess;
+    return false;
+};
+}
